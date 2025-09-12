1e4c0c54fd0dc71593bba0531f9724f88ef14cb4
ujjwaljain2112 <jainujjwal2112@gmail.com>
2025-09-12 19:18:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://onecart-frontend-33ex.onrender.com" , "http://localhost:5174"],
+ origin:["https://onecart-frontend-33ex.onrender.com" , "https://onecart-admin-zohb.onrender.com"],
  credentials:true
 }))
 
